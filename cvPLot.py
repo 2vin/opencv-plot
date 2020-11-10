fixes for scaling and time delay

cvPLot.py
@@ -13,6 +13,7 @@ class Plotter:
         self.plot_canvas = np.ones((self.height, self.width, 3))*255    
         self.ltime = 0
         self.plots = {}
+        self.plot_t_last = {}
         self.margin_l = 10
         self.margin_r = 10
         self.margin_u = 10
@@ -24,6 +25,7 @@ class Plotter:
     def plot(self, val, label = "plot"):
         if not label in self.plots:
             self.plots[label] = []
+            self.plot_t_last[label] = 0
             
         self.plots[label].append(int(val))
         while len(self.plots[label]) > self.sample_buffer:
@@ -39,9 +41,7 @@ class Plotter:
         scale_h_max = max(self.plots[label])
         scale_h_min = min(self.plots[label]) 
         scale_h_min = -scale_h_min if scale_h_min<0 else scale_h_min
-        #print(scale_h_max)
         scale_h = scale_h_max if scale_h_max > scale_h_min else scale_h_min
-        scale_h = scale_h if scale_h < self.sample_buffer else self.sample_buffer
         scale_h = ((self.height-self.margin_d-self.margin_u)/2)/scale_h if not scale_h == 0 else 0
         
 
@@ -51,24 +51,23 @@ class Plotter:
         
         
         cv2.rectangle(self.plot_canvas, (self.margin_l,self.margin_u), (self.width-self.margin_r,self.height-self.margin_d), (255,255,255), 1) 
-        cv2.putText(self.plot_canvas,f" {label} : {self.plots[label][-1]} , dt : {int((time.time() - self.ltime)*1000)}ms",(int(0),self.height-20),cv2.FONT_HERSHEY_SIMPLEX,0.6,(0,255,255),2)
+        cv2.putText(self.plot_canvas,f" {label} : {self.plots[label][-1]} , dt : {int((time.time() - self.plot_t_last[label])*1000)}ms",(int(0),self.height-20),cv2.FONT_HERSHEY_SIMPLEX,0.6,(0,255,255),2)
         cv2.circle(self.plot_canvas, (self.width-self.margin_r, int(self.margin_u + (self.height-self.margin_d-self.margin_u)/2 - self.plots[label][-1]*scale_h)), 2, (0,200,200), -1)
         
-        self.ltime = time.time()
+        self.plot_t_last[label] = time.time()
         cv2.imshow(label, self.plot_canvas)
         cv2.waitKey(1)
 
 
 def single_sample():
     # Create dummy values using for loop 
-    p = Plotter(400, 200,sample_buffer=1000)
+    p = Plotter(400, 200,sample_buffer=200)
 
     for v in range(1,3000):
         
-        p.plot(int(math.sin(v*3.14/180)*440),label='sin')
+        p.plot(int(math.sin(v*3.14/180)*100),label='sin')
         
         p.plot(int(math.cos(v*3.14/180)*50),label='cos')
-        #print(len(p.plots['sin']))
 
 
 